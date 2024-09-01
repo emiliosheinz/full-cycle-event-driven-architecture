9f185f22990903ae914c793cbe0ec4f529de3b1e
Emilio Heinzmann <emiliosheinz@gmail.com>
2024-09-01 10:10:12 -0300

    feat: start balance consumer microservice

---
+++ balance-consumer/src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config'
+
+import express from 'express'
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
